6db050dd0a99dde26df0f80d22f0c581ecdd875c
Santiago Semhan <santiagosemhan@gmail.com>
2019-09-16 09:44:33 -0300

    Trim data before save

--- cloud/classes/Base.js
+++ cloud/classes/Base.js
@@ -5,6 +5,14 @@ class Base extends Parse.Object {
   static beforeSave(request) {
     return new Promise((resolve, reject) => {
       const { user } = request;
+      Object.keys(request.object.attributes).forEach(function(attribute) {
+        const value = request.object.get(attribute);
+        if (typeof value === "string") {
+          request.object.set(attribute, value.trim());
+        }
+      });
+
+
       if(request.object.isNew()) {
         if (user) request.object.set("createdBy", user);
       } else {
