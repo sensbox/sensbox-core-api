132a0944d2487492eb22c86bd78b2708d63c2112
Santiago Semhan <santiagosemhan@gmail.com>
2020-05-19 18:11:57 -0300

    Added SENSBOX_DASHBOARD_ORIGIN env var. If it's set then ensure if the user has admin role

--- src/cloud/index.ts
+++ src/cloud/index.ts
@@ -1,4 +1,5 @@
 import { loadCloudFunctions, loadClassHooks } from './utils/loader';
+import SecurityService from './services/SecurityService';
 
 const Influx = require('influx');
 
@@ -17,8 +18,16 @@ const legacyMode = true;
 loadCloudFunctions(legacyMode);
 
 Parse.Cloud.beforeLogin(async (request: Parse.Cloud.TriggerRequest) => {
-  const { object: user } = request;
-  if (user.get('isBanned')) {
-    throw new Error('Access denied, your account is disabled.');
+  const { object: user, headers } = request;
+  const { origin } = headers;
+  try {
+    if (origin && origin === process.env.SENSBOX_DASHBOARD_ORIGIN) {
+      await SecurityService.ensureIsAdmin(<Parse.User>user);
+    }
+    if (user.get('isBanned')) {
+      throw new Error('Access denied, your account is disabled.');
+    }
+  } catch (error) {
+    throw new Parse.Error(Parse.Error.OPERATION_FORBIDDEN, error.message);
   }
 });
