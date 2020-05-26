e32edb9dcaddadceb740c692f2f7ce688c106222
Santiago Semhan <santiagosemhan@gmail.com>
2020-05-25 21:06:42 -0300

    Added publicServerUrl setting to parse server configuration.

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,12 @@ import { nullParser } from './cloud/utils';
 
 const port = process.env.PORT || 4444;
 const serverURL = process.env.CORE_URL || `http://localhost:${port}/parse`;
+const publicServerURL = process.env.PUBLIC_SERVER_URL || serverURL;
 
 export default {
   port,
   serverURL, // Don't forget to change to https if needed
+  publicServerURL,
   databaseURI: process.env.MONGO_DSN, // Connection string for your MongoDB database
   appId: process.env.APP_ID,
   masterKey: process.env.PARSE_SERVER_MASTER_KEY, // Keep this key secret!
